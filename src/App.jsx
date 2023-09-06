026def4f8ff2bfb492e5ee07a17cab4941436c1c
anthonralph <143595865+anthonralph@users.noreply.github.com>
2023-09-06 17:29:27 +0800

    add components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import ContactForm from "./views/Forms/ContactForm";
+import Card from "@mui/material/Card";
+import ContactList from "./views/List/ContactList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card sx={{ maxWidth: 400, padding: "20px" }}>
+        <h2>Add Contact</h2>
+        <ContactForm></ContactForm>
+      </Card>
+      <ContactList></ContactList>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
