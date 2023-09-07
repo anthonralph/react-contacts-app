c4dd377288669cccd7f6fd2f42788756083cb8c1
anthonralph <143595865+anthonralph@users.noreply.github.com>
2023-09-07 19:03:25 +0800

    create context and reducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import Card from "@mui/material/Card";
 import ContactForm from "./views/Forms/ContactForm";
 import ContactList from "./views/List/ContactList";
+import { ContactProvider } from "./ContactContext";
 
 function App() {
   return (
     <>
-      <Card sx={{ maxWidth: 400, padding: "20px" }}>
-        <h2>Add Contact</h2>
-        <ContactForm></ContactForm>
-      </Card>
-      <ContactList></ContactList>
+      <ContactProvider>
+        <Card sx={{ maxWidth: 400, padding: "20px" }}>
+          <h2>Add Contact</h2>
+          <ContactForm></ContactForm>
+        </Card>
+        <ContactList></ContactList>
+      </ContactProvider>
     </>
   );
 }
---
+++ src/ContactContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useReducer, useContext, useEffect } from "react";
+import { fetchContacts } from "./api";
+
+const ContactContext = createContext();
+
+const initialState = {
+  contacts: [],
+  isLoading: false,
+  editingId: "",
+};
+
+const contactReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_CONTACTS":
+      return {
+        contacts: action.payload,
+      };
+    case "SET_LOADING":
+      return {
+        isLoading: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const ContactProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  const fetchData = async () => {
+    const contactsData = await fetchContacts();
+    dispatch({ type: "SET_CONTACTS", payload: contactsData });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <ContactContext.Provider value={{ state, dispatch }}>
+      {children}
+    </ContactContext.Provider>
+  );
+};
+
+export const useContacts = () => {
+  const context = useContext(ContactContext);
+  if (!context) {
+    throw new Error("useContacts must be used within a ContactProvider");
+  }
+  return context;
+};
